3ab4b83c5fb73179e7328e64549e89945e04b719
gyojincompany <luckyred@gmail.com>
2024-01-24 11:38:04 +0900

    jpa설정 및 entity 클래스 추가

---
+++ src/test/java/com/gyojincompany/home/JpaTest20240124ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTest20240124ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
